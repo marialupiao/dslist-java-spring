e9d6f0ad659895a14bab92ed5e267ed428a0a458
Maria <marialupiao@outlook.com>
2023-05-10 18:58:03 -0300

    get lists

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -1,15 +1,17 @@
 package com.devsuperior.dslist.controllers;
 
-import com.devsuperior.dslist.dto.GameDTO;
-import com.devsuperior.dslist.dto.GameMinDTO;
-import com.devsuperior.dslist.services.GameService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.devsuperior.dslist.dto.GameDTO;
+import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.services.GameService;
+import com.devsuperior.dslist.services.GameListService;
 
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/games")
